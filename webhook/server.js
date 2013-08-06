5156f0570db10036e5a95cf02270f7f9e532b63f
Parker Moore <parkrmoore@gmail.com>
2013-08-06 14:07:05 +0200

    Parse and log POST body

--- webhook/server.js
+++ webhook/server.js
@@ -5,12 +5,24 @@ var closeWithStatusCode = function(res, status) {
   res.end('{}');
 };
 
+var parseBody = function(req, callback){
+  var buffer = '';
+  req.on("data", function(chunk){
+    buffer += chunk;
+  });
+  req.on("end", function(){
+    callback(JSON.stringify(buffer.toString()));
+  });
+}
+
 http.createServer(function (req, res) {
   if(/\/mirror/.test(req.url)) {
     if(req.method !== "POST") {
       closeWithStatusCode(res, 405);
     } else {
-      console.log(req);
+      parseBody(req, function(body){
+        console.log(body);
+      });
       closeWithStatusCode(res, 201);
     }
   } else {
