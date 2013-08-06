2d37bfe29e6d6fd7f82d6ea65e2c6a2e44818b23
Parker Moore <parkrmoore@gmail.com>
2013-08-06 14:30:22 +0200

    Add s3cmd to install script

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get -q -y install python-software-properties python g++ make upstart rsync
+sudo apt-get -q -y install python-software-properties python g++ make upstart rsync s3cmd
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -q -y install nodejs
